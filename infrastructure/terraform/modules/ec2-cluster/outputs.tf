a4d770df2e106c27677d6f7070b727b4764978d6
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-04-15 14:35:15 +0100

    feat(ec2-cluster): add private IP outputs
    Added outputs for private IPs of brokers and benchmark clients.
    
    This change allows users to access private IP addresses for better network management.

--- infrastructure/terraform/modules/ec2-cluster/outputs.tf
+++ infrastructure/terraform/modules/ec2-cluster/outputs.tf
@@ -2,6 +2,14 @@ output "broker_public_ips" {
   value = aws_instance.broker[*].public_ip
 }
 
+output "broker_private_ips" {
+  value = aws_instance.broker[*].private_ip
+}
+
 output "benchmark_client_public_ip" {
   value = aws_instance.benchmark_client.public_ip
+}
+
+output "benchmark_client_private_ip" {
+  value = aws_instance.benchmark_client.private_ip
 }
